933b96969478e5a07ec814157d182d236611840f
Angel Ngulumbi <angulumbi@middlebury.edu>
2020-07-12 22:59:52 -0400

    Updates

---
+++ precache-manifest.1b3ce4c97b05220eb2b1afec06b183bf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ae375a6873ff595892c0452f4020a0",
+    "url": "/rock-paper-scissors-master/index.html"
+  },
+  {
+    "revision": "b0f5361567c0f88e733d",
+    "url": "/rock-paper-scissors-master/static/css/main.b50f4974.chunk.css"
+  },
+  {
+    "revision": "f948a577ba3f6e0fe1d2",
+    "url": "/rock-paper-scissors-master/static/js/2.e12e6650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors-master/static/js/2.e12e6650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f5361567c0f88e733d",
+    "url": "/rock-paper-scissors-master/static/js/main.d36734af.chunk.js"
+  },
+  {
+    "revision": "21d627855a7c19b2e70f",
+    "url": "/rock-paper-scissors-master/static/js/runtime-main.92623ad4.js"
+  },
+  {
+    "revision": "a96c1e91dadf04f8955019c6b9db916f",
+    "url": "/rock-paper-scissors-master/static/media/bg-triangle.a96c1e91.svg"
+  },
+  {
+    "revision": "35c20f13fe98ae34395372270324381c",
+    "url": "/rock-paper-scissors-master/static/media/blue-circle.35c20f13.svg"
+  },
+  {
+    "revision": "d1463231c7ce44e3ffb4c8bbeb8fe793",
+    "url": "/rock-paper-scissors-master/static/media/dot.d1463231.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors-master/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors-master/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors-master/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors-master/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors-master/static/media/image-rules.5d74d25d.svg"
+  },
+  {
+    "revision": "759a207509c8dad61b7e365b27e0d0bd",
+    "url": "/rock-paper-scissors-master/static/media/logo.759a2075.svg"
+  },
+  {
+    "revision": "9d6e221241c05fdded0aeebc01bb77d7",
+    "url": "/rock-paper-scissors-master/static/media/red-circle.9d6e2212.svg"
+  },
+  {
+    "revision": "d9074218369a1a39e40f52b4b8f175a2",
+    "url": "/rock-paper-scissors-master/static/media/yellow-circle.d9074218.svg"
+  }
+]);
